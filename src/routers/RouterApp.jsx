15e588506bc3d6ffa8dd42be7705469fd3e930b1
ramiroSicilia <moto2.belgrano@gmail.com>
2025-12-09 17:06:32 -0300

    mensaje

--- src/routers/RouterApp.jsx
+++ src/routers/RouterApp.jsx
@@ -1,38 +1,37 @@
 
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
-import Layout from "../Layout/Layout";
+import { Routes,Route } from "react-router-dom";
+import Layout from "../Layout/Layout"
 import Services from "../Pages/Services";
 import Shifts from "../Pages/Shifts";
 import Contact from "../Pages/Contact";
 import Projects from "../Pages/Projects";
 import { Paquetes } from '../Pages/Paquetes';
-import { ScrollToTop } from "../Components/ScrollToTop";
+import {ScrollToTop} from "../Components/ScrollToTop"
+
+
+
 
 const RouterApp = () => {
   return (
     <> 
-      <ScrollToTop/> 
+         <ScrollToTop/> 
+         
+          <Routes>
+
+            <Route path='/' element={<Layout/>}>
+            <Route path='services' element={<Services/>}></Route>
+            <Route path='shifts' element={<Shifts/>}></Route>
+            <Route path='projects' element={<Projects/>}></Route>
+            <Route path='contact' element={<Contact/>}></Route> 
+             <Route path='services/paquetes'element={<Paquetes/>}></Route>
+            </Route> 
+
+
+           
+
+           </Routes>
        
-      <Routes>
-
-        <Route path='/' element={<Layout />}>
-
-          {/* Services */}
-          <Route path='services' element={<Services />} />
-
-          {/* Paquetes dentro de services */}
-          <Route path='services/paquetes' element={<Paquetes />} />
-
-          {/* Otras páginas */}
-          <Route path='shifts' element={<Shifts />} />
-          <Route path='projects' element={<Projects />} />
-          <Route path='contact' element={<Contact />} />
-
-        </Route>
-
-      </Routes>
-    
     </>
   );
 }
